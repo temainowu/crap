0b5b85fc0d24d244ea53e59f1e3d64255a6e9666
temainowu <69645302+temainowu@users.noreply.github.com>
2026-02-13 13:57:54 +0000

    added their original code
    + Function that checks you get the same result for both implementations

--- fluxions.hs
+++ fluxions.hs
@@ -232,12 +232,24 @@ divide (F (xs,n) :/ F (ys,m)) = divide (F (xs,n-m) :/ F (ys,0))
 
 -- Adapted code from error-code-864g:
 
+root_newton x f f' tolerance 0 = 0
+root_newton x f f' tolerance n = if abs (- f x/f' x) < tolerance
+        then x - f x/f' x
+        else root_newton (x - f x/f' x) f f' tolerance (n-1)
+
+root1 = root_newton 1 (\x -> x^2-x-1) (\x -> 2*x - 1) (1/10^20) 1000
+
 rootNewton :: (Fractional n, Ord n) => n -> (Fluxion n -> Fluxion n) -> n -> Int -> Maybe n
 rootNewton x f tolerance 0 = Just 0
 rootNewton x f tolerance n = case diff f x of
-    Just z -> if abs (-(lim (f x')/z)) < tolerance then Just (x - (lim (f x')/z)) else rootNewton (x - lim (f x')/z) f tolerance (n-1)
+    Just z -> if abs (-(lim (f x')/z)) < tolerance then Just (x - (lim (f x')/z)) else rootNewton (x - (lim (f x')/z)) f tolerance (n-1)
     Nothing -> Nothing
     where x' = F ([x],0)
 
 root :: (Fractional a, Ord a) => Maybe a
 root = rootNewton 1 (\x -> x^2-x-1) (1/10^20) 1000
+
+rootDifference :: (Eq a, Fractional a, Ord a) => a -> (Fluxion a -> Fluxion a) -> (a -> a) -> a -> Int -> Maybe a
+rootDifference x f f' tol n = case rootNewton x f tol n of 
+        Just x -> Just (x - root_newton x (lim . f . \x -> F ([x],0)) f' tol n)
+        Nothing -> Nothing
